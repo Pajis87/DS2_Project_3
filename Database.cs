78a9a94051be2535d505a0127b22c7de4bc3354d
Pavel Brázda <bra0241@vsb.cz>
2026-05-06 13:45:23 +0200

    init

---
+++ Database.cs
@@ -0,0 +1,135 @@
+﻿using System.Configuration;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace DS2_Project_3
+{
+    internal class Database
+    {
+        private OracleConnection Connection { get; set; }
+        private OracleTransaction SqlTransaction { get; set; }
+        public string Language { get; set; }
+
+        public Database()
+        {
+            Connection = new OracleConnection();
+            Language = "en";
+        }
+
+        /// <summary>
+        /// Connect
+        /// </summary>
+        public bool Connect(String conString)
+        {
+            if (Connection.State != ConnectionState.Open)
+            {
+                Connection.ConnectionString = conString;
+                Connection.Open();
+            }
+            return true;
+        }
+        
+        /// <summary>
+        /// Connect
+        /// </summary>
+        public bool Connect()
+        {
+            bool ret = true;
+
+            if (Connection.State != ConnectionState.Open)
+            {
+                ret = Connect(ConfigurationManager.ConnectionStrings["ConnectionStringOracle"].ConnectionString);
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Close.
+        /// </summary>
+        public void Close()
+        {
+            Connection.Close();
+        }
+
+        /// <summary>
+        /// Begin a transaction.
+        /// </summary>
+        public void BeginTransaction()
+        {
+            SqlTransaction = Connection.BeginTransaction(IsolationLevel.Serializable);
+        }
+
+        /// <summary>
+        /// End a transaction.
+        /// </summary>
+        public void EndTransaction()
+        {
+            // command.Dispose()
+            SqlTransaction.Commit();
+            Close();
+        }
+
+        /// <summary>
+        /// If a transaction is failed call it.
+        /// </summary>
+        public void Rollback()
+        {
+            SqlTransaction.Rollback();
+        }
+
+        /// <summary>
+        /// Insert a record encapulated in the command.
+        /// </summary>
+        public int ExecuteNonQuery(OracleCommand command)
+        {
+            int rowNumber = 0;
+            rowNumber = command.ExecuteNonQuery();
+            return rowNumber;
+        }
+
+        /// <summary>
+        /// Create command.
+        /// </summary>
+        public OracleCommand CreateCommand(string strCommand)
+        {
+            OracleCommand command = new OracleCommand(strCommand, Connection);
+
+            if (SqlTransaction != null)
+            {
+                command.Transaction = SqlTransaction;
+            }
+            return command;
+        }
+        /// <summary>
+        /// Select encapulated in the command.
+        /// </summary>
+        public OracleDataReader Select(OracleCommand command)
+        {
+            //command.Prepare();
+            OracleDataReader sqlReader = command.ExecuteReader();
+            return sqlReader;
+        }
+        public static Database Connect(Database pDb)
+        {
+            Database db;
+            if (pDb == null)
+            {
+                db = new Database();
+                db.Connect();
+            }
+            else
+            {
+                db = pDb;
+            }
+            return db;
+        }
+        public static void Close(Database pDb, Database db)
+        {
+            if (pDb == null)
+            {
+                db.Close();
+            }
+        }
+    }
+}
---
+++ orm/dto/Vycvik.cs
@@ -0,0 +1,15 @@
+﻿namespace DS2_Project_3
+{
+    public class Vycvik
+    {
+        public int vId { get; set; }
+        public string? poznamky { get; set; }
+        public DateTime cas_od { get; set; }
+        public DateTime cas_do { get; set; }
+    public int pocetMist { get; set; }
+        public int pocetVolnychMist { get; set; }
+        public Trener trener { get; set; }
+        public Misto misto { get; set; }
+
+    }
+}
